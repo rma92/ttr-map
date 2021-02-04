f2639f5f92785974396b04c46bdb465ba33e1fa8
administrator <administrator@example.com>
2021-02-03 19:40:03 -0500

    Implemented lat lon to x y algorithm

--- ttm.js
+++ ttm.js
@@ -1,4 +1,24 @@
 /*
+  Given a longitude, uses left_x, right_x, and left_lon and right_lon to
+  get the screen x coordinate.
+*/
+function lon_to_x(lon)
+{
+  lon_width = right_lon - left_lon;
+  x_width = right_x - left_x;
+  units = x_width / lon_width; //pixels per line of latitude
+  return units * (lon - left_lon) + left_x;
+}
+
+function lat_to_y(lat)
+{
+  lat_height = top_lat - bottom_lat;
+  y_height = top_y - bottom_y;
+  units = y_height / lat_height;
+  return units * (lat - top_lat) + top_y;
+}
+
+/*
   iterate over the city list, add a button for each city at the correct place.
 */
 function createDOMCities()
@@ -6,6 +26,13 @@ function createDOMCities()
   for( const [key, value] of Object.entries(cities))
   {
     console.log(key + ": " + value.name);
+    var b = document.createElement('BUTTON');
+    var textB = document.createTextNode(value.name);
+    b.appendChild(textB);
+    b.style.position = "absolute";
+    b.style.left = lon_to_x(value.lon) + "px";
+    b.style.top = lat_to_y(value.lat) + "px";
+    document.body.appendChild(b);
   }
 }
 
--- ttmdata.js
+++ ttmdata.js
@@ -14,12 +14,12 @@ cities['LAX'] = new City('Los Angeles', 33.94, -118.40);
 cities['BOS'] = new City('Boston', 42.36, -71.00);
 cities['MIA'] = new City('Miami', 25.80, -80.29);
 
-var left_lat = -125;
-var right_lat = -69;
-var top_lon = 51;
-var bottom_lon = 23;
+var left_lon = -125;
+var right_lon = -69;
+var top_lat = 51;
+var bottom_lat = 23;
 
 var left_x = 10; //the left margin for the board
 var right_x = 810; //the right margin for the board
 var top_y = 10; //the top margin for the board
-var bot_y = 510; //the bottom margin for the board
+var bottom_y = 510; //the bottom margin for the board
